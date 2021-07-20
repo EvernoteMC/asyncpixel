d055fc8f7e4a25f8ceba969263b48d7af97febea
Evernote <evernoteminecraft@gmail.com>
2021-07-20 13:56:20 +0530

    Changed class names

--- asyncpixel/models/players/__init__.py
+++ asyncpixel/models/players/__init__.py
@@ -1,48 +1,46 @@
 """Player Stats."""
 from .arcade import Arcade
-from .arena import Arena
-from .battleground import Battleground
+from .arena_brawl import ArenaBrawl
 from .bedwars import Bedwars
-from .bedwars import BedwarsGame
+from .blitz_survival_games import BlitzSurvivalGames
 from .build_battle import BuildBattle
+from .cops_and_crims import CopsAndCrims
 from .duels import Duels
-from .gingerbread import GingerBread
-from .housing import Housing
-from .hunger_games import HungerGames
-from .legacy import Legacy
-from .mcgo import MCGO
+from .mega_walls import MegaWalls
+from .murder_mystery import MurderMystery
 from .paintball import Paintball
 from .pit import Pit
-from .quake import Quake
-from .skyclash import SkyClash
-from .skywars import Skywars
+from .quakecraft import Quakecraft
+from .skywars import SkyWars
+from .smash_heroes import SmashHeroes
+from .speed_uhc import SpeedUHC
 from .tnt_games import TNTGames
+from .turbo_kart_racers import TurboKartRacers
 from .uhc import UHC
 from .vampirez import VampireZ
 from .walls import Walls
-from .walls3 import Walls3
+from .warlords import Warlords
 
 __all__ = [
     "Arcade",
-    "Arena",
-    "Battleground",
+    "ArenaBrawl",
     "Bedwars",
+    "BlitzSurvivalGames",
     "BuildBattle",
+    "CopsAndCrims",
     "Duels",
-    "GingerBread",
-    "Housing",
-    "HungerGames",
-    "Legacy",
-    "MCGO",
+    "MegaWalls",
+    "MurderMystery",
     "Paintball",
     "Pit",
-    "Quake",
-    "SkyClash",
+    "Quakecraft",
+    "SkyWars",
+    "SmashHeroes",
+    "SpeedUHC",
     "TNTGames",
+    "TurboKartRacers",
     "UHC",
     "VampireZ",
     "Walls",
-    "Walls3",
-    "BedwarsGame",
-    "Skywars",
+    "Warlords",
 ]
